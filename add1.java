79288985fe3ca3b2f8fca57690d1f0ef13b7ad76
Nikitha26reddy <245122749061@mvsrec.edu.in>
2025-03-25 15:54:51 +0530

    Update add1.java

--- add1.java
+++ add1.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
-class add1{
-        public static void main(String args[])
-        {
-                int x,y;
-                int c;
-		Scanner myObj = new Scanner(System.in); 
-    		System.out.println("Type a number:");
-    		x = myObj.nextInt(); 
-
-    		System.out.println("Type another number:");
-    		y = myObj.nextInt(); 
-		c=x+y;
-                System.out.println("x+y=");
-                System.out.println(c);
+public class add1 {
+    public static void main(String[] args) {
+        // Check if exactly two arguments are provided
+        if (args.length != 2) {
+            System.out.println("Please provide exactly two numbers as command-line arguments.");
+            return;
         }
+
+        try {
+            // Parse the command-line arguments to integers
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+
+            // Calculate the sum
+            int sum = num1 + num2;
+
+            // Print the result
+            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        } catch (NumberFormatException e) {
+            System.out.println("Please provide valid integers as command-line arguments.");
+        }
+    }
 }
 
