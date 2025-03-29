3fc0c414a1eedc18d7e4bf67a5fee6bce8e862a0
Nikitha26reddy <245122749061@mvsrec.edu.in>
2025-03-29 15:04:21 +0530

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,14 +1,13 @@
-import java.util.*;
+
 
 public class UserInfo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
-        String rollNumber = scanner.nextLine();
-        String departmentName = scanner.nextLine();
-        System.out.println( username);
-        System.out.println(rollNumber);
-        System.out.println(departmentName);
+        String a1=args[0];
+        String a2=args[1];
+        String a3=args[2];
+        System.out.println( a1);
+        System.out.println(a2);
+        System.out.println(a3);
     }
 }
 
